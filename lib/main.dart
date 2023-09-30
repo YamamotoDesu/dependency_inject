24671728ec1bfdd8c793ec655fb1d66e17ce959c
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-09-30 18:35:17 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+import 'app_service.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final AppService _appService = AppService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'FLUTTER GUYS',
+            style: TextStyle(fontSize: 14),
+          ),
+          backgroundColor: const Color(0xff1D1E22)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            OutlinedButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              ),
+              onPressed: () {
+                String currentDate = _appService.execute();
+                showSnackBar(currentDate, context);
+              },
+              child: const Text('Get Date'),
+            ),
+            OutlinedButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              ),
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const DetailsPage()),
+                );
+              },
+              child: const Text('Details page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Details Page',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.grey[500],
+        elevation: 1,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text('Welcome to our DI tutorial'),
+          ),
+          OutlinedButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+            ),
+            onPressed: () {},
+            child: const Text('Get Date'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void showSnackBar(String currentDate, BuildContext context) {
+  SnackBar snackBar = SnackBar(
+    content: Text(currentDate),
+    duration: const Duration(milliseconds: 500),
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
